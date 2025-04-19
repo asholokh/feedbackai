9d0605eee9fda5658142ef456e07a9e4a8065c94
Andrii Sholokh <asuper@gmail.com>
2025-04-19 17:10:27 +0200

    Extracted Firebase API auth details to environment variables

--- firebase/firebaseConfig.ts
+++ firebase/firebaseConfig.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "feedbackai-230ef.firebaseapp.com",
-    projectId: "feedbackai-230ef",
-    storageBucket: "feedbackai-230ef.firebasestorage.app",
-    messagingSenderId: "552972462322",
-    appId: "1:552972462322:web:135c3ec01e762b4173886b",
-    measurementId: "G-WPL94PQCJ9"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
